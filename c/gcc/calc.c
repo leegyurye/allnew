3d4df9a4917af4acb07a2cee88eb379462d22da1
leegyurye <127065157+leegyurye@users.noreply.github.com>
2023-03-16 15:04:48 +0900

    d

--- c/gcc/calc.c
+++ c/gcc/calc.c
@@ -3,7 +3,7 @@
 
 void main() {
   int x, y;
-  printf("Input two numbers : ")
+  printf("Input two numbers : ");
   scanf("%d %d", &x, &y);
   printf("%d + %d = %d\n", x, y, plus(x, y));
   printf("%d - %d = %d\n", x, y, minus(x, y));

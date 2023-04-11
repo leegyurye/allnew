32da6e5248cac330f6469021a7f3b1adb04cf63b
leegyurye <lgr0813@naver.com>
2023-04-11 12:02:29 +0900

    update

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('sync-mysql');
-const env = require('dotenv').config({ path: "../../.env "});
+const env = require('dotenv').config({ path: "../../.env" });
 
-var connection = new mysql ({
+var connection = new mysql({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database 
+    database: process.env.database
 });
 
 const app = express()
@@ -18,7 +18,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/hello', (req, res) => {
-    res.end('hello world~!!')
+    res.send('Hello World~!!')
 })
 
-module.express = app;
+app.get('/select', (req, res) => {
+    const result = connection.query('select * from user');
+    console.log(result)
+    res.send(result)
+})
+
+app.post('/select', (req, res) => {
+    const result = connection.query('select * from user');
+    console.log(result)
+    res.send(result)
+})
+
+
+app.get('/selectQuery', (req, res) => {
+    const userid = req.query.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result);
+})
+
+app.post('/selectQuery', (req, res) => {
+    const userid = req.body.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result);
+})
+
+module.exports = app;

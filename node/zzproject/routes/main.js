09023ad34320fa2075dab28651f7a89923d070b0
leegyurye <lgr0813@naver.com>
2023-04-26 20:13:10 +0900

    project

--- node/zzproject/routes/main.js
+++ node/zzproject/routes/main.js
@@ -120,19 +120,19 @@ function template_result3(result, res) {
 // 로그인
 app.post('/login', (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query("select * from usertbl where userid=? and passwd=?", [id, pw]);
-    // console.log(result);
-    if (result.length == 0) {
+    const usertbl = connection.query("select * from usertbl where userid=? and passwd=?", [id, pw]);
+    // console.log(usertbl);
+    if (usertbl.length == 0) {
         res.redirect('error.html')
     }
     if (id == 'admin' || id == 'root') {
         console.log(id + " => Administrator Logined")
         // res.redirect('member.html?id=' + id)
-        res.send({ "ok": true, "id": [id], "service": "login" });
+        res.send({ "ok": true, "userid": [id], "service": "Admin login" });
     } else {
         console.log(id + " => User Logined")
         // res.redirect('main.html?id=' + id)
-        res.send({ "ok": true, "id": [id], "service": "login" });
+        res.send({ "ok": true, "userid": [id], "service": "User login" });
     }
 })
 
@@ -142,8 +142,8 @@ app.post('/register', (req, res) => {
     if (id == "") {
         res.redirect('register.html')
     } else {
-        let result = connection.query("select * from usertbl where userid=? and passwd=? and userName=? and userAddr=? and userNumber=?", [id, pw, name, addr, num]);
-        if (result.length > 0) {
+        let usertbl = connection.query("select * from usertbl where userid=? and passwd=? and userName=? and userAddr=? and userNumber=?", [id, pw, name, addr, num]);
+        if (usertbl.length > 0) {
             res.writeHead(200);
             var template = `
         <!doctype html>
@@ -163,9 +163,9 @@ app.post('/register', (req, res) => {
         `;
             res.end(template);
         } else {
-            result = connection.query("insert into usertbl values (?, ?, ?, ?, ?)", [id, pw, name, addr, num]);
-            console.log(result);
-            res.send({ "ok": true, "result": [id, pw, name, addr, num], "service": "register" });
+            usertbl = connection.query("insert into usertbl values (?, ?, ?, ?, ?)", [id, pw, name, addr, num]);
+            console.log(usertbl);
+            res.send({ "ok": true, "usertbl": [{ "id": id, "pw": pw, "name": name, "addr": addr, "num": num }], "service": "register" });
             // res.redirect('/');
         }
     }
@@ -179,31 +179,31 @@ app.get('/selectDong', (req, res) => {
         // res.send('원하는 동을 입력하세요.')
         res.write("<script>alert('원하는 동을 입력하세요')</script>");
     } else {
-        const result = connection.query("SELECT * FROM shoptbl where shopArea=?", [shopArea]);
-        console.log(result);
-        // res.send(result);
-        if (result.length == 0) {
+        const shoptbl = connection.query("SELECT * FROM shoptbl where shopArea=?", [shopArea]);
+        console.log(shoptbl);
+        // res.send(shoptbl);
+        if (shoptbl.length == 0) {
             // template_nodata(res);
-            res.send({ "ok": true, "result": [result], "service": "SelectDong" });
+            res.send({ "ok": false, "shopArea": [shopArea], "service": "SelectDong" });
         } else {
             // template_result2(result, res);
-            res.send({ "ok": true, "result": [result], "service": "SelectDong" });
+            res.send({ "ok": true, "shopArea": [shopArea], "service": "SelectDong" });
         }
     }
 })
 
 // 전체 업체 검색
 app.get('/select', (req, res) => {
-    const result = connection.query('SELECT * FROM shoptbl');
-    console.log(result);
-    //res.send('{"ok":true, "affectedRows":' + result.affectedRows + ', "service":"insert"}');
-    // res.send(result);
-    if (result.length == 0) {
+    const shoptbl = connection.query('SELECT * FROM shoptbl');
+    console.log(shoptbl);
+    //res.send('{"ok":true, "affectedRows":' + shoptbl.affectedRows + ', "service":"insert"}');
+    // res.send(shoptbl);
+    if (shoptbl.length == 0) {
         // template_nodata(res);
-        res.send({ "ok": true, "result": [result], "service": "select" });
+        res.send({ "ok": false, "shoptbl": [shoptbl], "service": "select" });
     } else {
         // template_result2(result, res);
-        res.send({ "ok": true, "result": [result], "service": "select" });
+        res.send({ "ok": true, "shoptbl": [shoptbl], "service": "select" });
     }
 
 })
@@ -213,10 +213,10 @@ app.post('/insert', (req, res) => {
     const { resNumber, userId, shopName, resDate, shopService, shopArea } = req.body;
     if (resNumber == "" || userId == "" || shopName == "" || resDate == "" || shopService == "" || shopArea == "") {
         // res.send('정보를 빠짐없이 입력하세요.')
-        res.write("<script>alert('정보를 빠짐없이 입력하세요.')</script>");
+        res.write({ "ok": false, "restbl": [restbl], "service": "Reservation" });
     } else {
-        let result = connection.query("select * from restbl where resNumber=?", [resNumber]);
-        if (result.length > 0) {
+        let restbl = connection.query("select * from restbl where resNumber=?", [resNumber]);
+        if (restbl.length > 0) {
             res.writeHead(200);
             var template = `
         <!doctype html>
@@ -235,25 +235,25 @@ app.post('/insert', (req, res) => {
         `;
             res.end(template);
         } else {
-            result = connection.query("insert into restbl values (?, ?, ?, ?, ?, ?)", [resNumber, userId, shopName, resDate, shopService, shopArea]);
-            console.log(result);
+            restbl = connection.query("insert into restbl values (?, ?, ?, ?, ?, ?)", [resNumber, userId, shopName, resDate, shopService, shopArea]);
+            console.log(restbl);
             // res.redirect('/selectQuery?resNumber=' + req.body.resNumber);
-            res.send({ "ok": true, "result": [result], "service": "insert" });
+            res.send({ "ok": true, "restbl": [resNumber, userId, shopName, resDate, shopService, shopArea], "service": "Reservation" });
         }
     }
 })
 
-// request O, query X
+// ReservationSelect (예약 정보 출력)
 app.get('/select2', (req, res) => {
-    const result = connection.query('SELECT * FROM restbl');
-    console.log(result);
-    // res.send(result);
-    if (result.length == 0) {
+    const restbl = connection.query('SELECT * FROM restbl');
+    console.log(restbl);
+    // res.send(restbl);
+    if (restbl.length == 0) {
         // template_nodata(res);
-        res.send({ "ok": true, "result": [result], "service": "select" });
+        res.send({ "ok": false, "restbl": [restbl], "service": "ReservationSelect" });
     } else {
-        // template_result3(result, res);
-        res.send({ "ok": true, "result": [result], "service": "select" });
+        // template_result3(restbl, res);
+        res.send({ "ok": true, "restbl": [restbl], "service": "ReservationSelect" });
     }
 })
 
@@ -274,6 +274,7 @@ var Restbls = mongoose.model('restbls', restblSchema);
 // mongo insert
 app.post('/mongoinsert', function (req, res) {
     let result = resselect_result(req)
+    //상위에 지정한 My SQL > Mongo로 이동하는 Function 이므로 result로 변수 선언
     let flag = 0
 
     for (var i = 0; i < result.length; i++) {
@@ -297,18 +298,19 @@ app.post('/mongoinsert', function (req, res) {
     if (flag) {
         console.log('err')
         // res.status(500).send('insert error')
-        res.send({ "ok": true, "result": [result], "service": "mongoinsert" });
+        res.send({ "ok": false, "mongoinsert": [restbls], "service": "mongoinsert" });
     } else {
         // res.status(200).send("Inserted")
-        res.send({ "ok": true, "result": [result], "service": "mongoinsert" });
+        res.send({ "ok": true, "mongoinsert": [restbls], "service": "mongoinsert" });
     }
+
 })
 
 // list
 app.get('/mongolist', function (req, res, next) {
-    Restbls.find({}, function (err, docs) {
+    Restbls.find({}, function (err, mongolist) {
         if (err) console.log('err')
-        res.send(docs)
+        res.send({ "ok": true, "mongolist": [mongolist], "service": "mongoinsert" })
 
     })
 })
@@ -326,7 +328,7 @@ app.post('/mongoupdate', function (req, res, next) {
         if (err) {
             console.log('err')
             // res.status(500).send('update error')
-            res.status(500).send('update error')
+            res.status(500).send({ "ok": false, "mongoupdate": [restbl], "service": "mongoupdate" })
             return;
         }
         restbl.userId = userId;
@@ -338,11 +340,11 @@ app.post('/mongoupdate', function (req, res, next) {
         restbl.save(function (err, silence) {
             if (err) {
                 console.log('err')
-                res.status(500).send('update error')
+                res.status(500).send({ "ok": false, "mongoupdate": [restbl], "service": "mongoupdate" })
                 return;
             }
             // res.status(200).send("Updated")
-            res.status(200).send({ "ok": true, "result": [restbl], "service": "mongoupdate" })
+            res.status(200).send({ "ok": true, "mongoupdate": [restbl], "service": "mongoupdate" })
 
         })
     })
@@ -356,11 +358,11 @@ app.post('/mongodelete', function (req, res, next) {
     restbls.remove(function (err) {
         if (err) {
             console.log('err')
-            res.status(500).send('delete error')
+            res.status(500).send({ "ok": false, "mongodelete": [restbls], "service": "mongodelete" })
             return;
         }
         // res.status(200).send("Removed")
-        res.status(200).send({ "ok": true, "service": "mmongodelete" })
+        res.status(200).send({ "ok": false, "mongodelete": [restbls], "service": "mongodelete" })
     })
 })
 

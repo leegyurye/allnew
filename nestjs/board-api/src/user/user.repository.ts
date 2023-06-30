5f8aba6bf2f2751a891659e30e4d26f18cb4d9d8
leegyurye <127065157+leegyurye@users.noreply.github.com>
2023-06-30 15:30:30 +0900

    Delete nestjs/board-api directory

--- nestjs/board-api/src/user/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { User } from "../entities/user.entity";
-
-@EntityRepository(User)
-export class UserRepository extends 
-Repository<User> {
-}

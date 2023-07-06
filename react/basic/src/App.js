7f3d021d757af5d10d169c40c5e547b409dd7583
leegyurye <lgr0813@naver.com>
2023-07-06 03:22:45 +0000

    react

--- react/basic/src/App.js
+++ react/basic/src/App.js
@@ -1,14 +1,63 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper'
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  })
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'developer',
+      email: 'public.developer@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'public.tester@gmail.com'
+    },
+    {
+      id: 3,
+      username: 'moon',
+      email: 'public.moon@gmail.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    })
+    nextId.current += 1;
+  }
+
   return (
-    <Wrapper>
-    <Hello name="react" color="red" />
-    <Hello color="pink" />
-    </Wrapper>
-    )
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
+    </>
+  )
 }
 
 export default App;
